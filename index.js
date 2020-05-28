df814b52a22fcb6c1094e93bf2dab24a31a0b8ac
BillelBenoudjit <hb_benoudjit@esi.dz>
2020-05-28 11:19:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const body_parser = require("body-parser")
+
+app.use(body_parser.urlencoded({extended: false}))
+app.use(body_parser.json())
+
+const port = process.env.PORT || 8000 
+/*const routes = require("./app/Routes/routes")
+routes(app)
+
+const db = require("./app/Config/db")
+db.InitiateMongoServer()*/
+
+app.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+})
