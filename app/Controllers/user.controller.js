ddf358ef336b0d7391f377911ae5bc3884b9abbf
BillelBenoudjit <hb_benoudjit@esi.dz>
2020-05-30 19:14:10 +0100

    Signup & signin

---
+++ app/Controllers/user.controller.js
@@ -0,0 +1,84 @@
+const User = require('../Models/user.model')
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+exports.signup = async (req, res) => {
+    try {
+        user = new User({
+            email: req.body.email,
+            password: req.body.password,
+            userName: req.body.userName
+        })
+
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(user.password, salt)
+
+        await user.save()
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            "randomString", {
+            expiresIn: 10000
+        },
+            (err, token) => {
+                if (err) throw err
+                res.status(200).json({
+                    token,
+                    user
+                })
+            }
+        )
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send("Error in Saving")
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        let user = await User.findOne({
+            'email': req.body.email
+        })
+        if (!user)
+            return res.status(400).json({
+                message: "user Not Exist"
+            })
+
+        const isMatch = await bcrypt.compare(req.body.password, user.password)
+        if (!isMatch)
+            return res.status(400).json({
+                message: "Incorrect Password !"
+            })
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            "secret",
+            {
+                expiresIn: 3600
+            },
+            (err, token) => {
+                if (err) throw err
+                res.status(200).json({
+                    token,
+                })
+            }
+        )
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({
+            message: "Server Error"
+        })
+    }
+}
--- app/Routes/routes.js
+++ app/Routes/routes.js
@@ -1,7 +1,11 @@
 const { check, validationResult } = require("express-validator/check")
+const userController = require("../Controllers/user.controller")
 
 module.exports = (app) => {
     app.get("/", (req, res) => {
         res.json("Welcome to our app API !")
     })
+    //User routes
+    app.post("/signup", userController.signup)
+    app.post("/login", userController.login)
 }
