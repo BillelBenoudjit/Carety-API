09c465c5e495fe778b122edd30365a43b3c145a8
BillelBenoudjit <hb_benoudjit@esi.dz>
2020-05-30 18:58:46 +0100

    Add Procfile to deploy

--- app/Routes/routes.js
+++ app/Routes/routes.js
@@ -1,8 +1,6 @@
 const { check, validationResult } = require("express-validator/check")
 
 module.exports = (app) => {
-    //Members routes
-
     app.get("/", (req, res) => {
         res.json("Welcome to our app API !")
     })
